f6268efb3727c391b5883cc0b0b58b8ef22a2c30
shoyonp <shoyonkumar143@gmail.com>
2024-12-29 21:38:22 +0600

    dynamic tite done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,22 +1,30 @@
-import React, { Children } from 'react';
-import Banner from './Banner/Banner';
-import Category from './Category/Category';
-import Chef from './Chef/Chef';
-import PopularMenu from './PopularMenu/PopularMenu';
-import Featured from './Featured/Featured';
-import Testimonials from './Testimonials/Testimonials';
+import React, { Children } from "react";
+import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
+import Chef from "./Chef/Chef";
+import PopularMenu from "./PopularMenu/PopularMenu";
+import Featured from "./Featured/Featured";
+import Testimonials from "./Testimonials/Testimonials";
+import { Helmet } from "react-helmet-async";
 
 const Home = () => {
-    return (
-        <div>
-           <Banner></Banner>
-           <section className='w-9/12 mx-auto'><Category></Category></section>
-           <section className='mb-10'><Chef></Chef></section>
-           <PopularMenu></PopularMenu>
-           <Featured></Featured>
-           <Testimonials></Testimonials>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Home</title>
+      </Helmet>
+      <Banner></Banner>
+      <section className="w-9/12 mx-auto">
+        <Category></Category>
+      </section>
+      <section className="mb-10">
+        <Chef></Chef>
+      </section>
+      <PopularMenu></PopularMenu>
+      <Featured></Featured>
+      <Testimonials></Testimonials>
+    </div>
+  );
 };
 
 export default Home;
--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -1,24 +1,13 @@
+import { NavLink } from "react-router-dom";
+
 const NavBar = () => {
   const links = (
     <>
       <li>
-        <a>Item 1</a>
+        <NavLink to="/">Home</NavLink>
       </li>
       <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li>
-              <a>Submenu 1</a>
-            </li>
-            <li>
-              <a>Submenu 2</a>
-            </li>
-          </ul>
-        </details>
-      </li>
-      <li>
-        <a>Item 3</a>
+        <NavLink to="/menu">Menu</NavLink>
       </li>
     </>
   );
